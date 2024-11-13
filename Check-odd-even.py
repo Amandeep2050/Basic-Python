4d20078823d37fe78efcdda319a6064d61d11065
Amandeep2050 <amandeepramola638@gmail.com>
2024-11-13 20:04:02 +0530

    Checking whether a number is odd or even.

---
+++ Check-odd-even.py
@@ -0,0 +1,11 @@
+# Checking whether a number is odd or even.
+
+# Taking a number as input
+Num = int(input("Enter a Number: "))
+
+if(Num % 2 == 0):       # Checking divisibility of given number by 2
+    print("It's Even")
+else:
+    print("It's Odd")
+
+print("Thank you!")
