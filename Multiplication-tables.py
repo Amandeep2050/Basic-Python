deb1fb0df65b67e491e18361698a9e2f7d28e5a5
Amandeep2050 <amandeepramola638@gmail.com>
2024-11-14 12:07:13 +0530

    Printing table of a given number using while loop.

---
+++ Multiplication-tables.py
@@ -0,0 +1,12 @@
+# Printing table of a given number using while loop.
+
+# Taking a number as input
+n = int(input("Enter a number: "))
+i = 1 # iteration variable
+
+# Running While loop for 10 times
+while i<=10:                    # Condition
+    print(n ,"X",i,"=", n*i)    # Print table
+    i+=1                        # Update condtion
+
+print("Thankyou!")              # End of the code.
